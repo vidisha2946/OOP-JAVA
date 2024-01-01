88845722ee63748880bc02a3e51276f28be4454d
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2024-01-01 20:27:37 +0530

    Add files via upload

---
+++ java/Add.java
@@ -0,0 +1,8 @@
+public class Add{
+	public static void main(String[] args){
+		System.out.println("Vidisha Bhgaiya");
+		System.out.println("shree ram park-2");
+		System.out.println("kuwadva road");
+		System.out.println("rajkot-360003");
+	}
+}
