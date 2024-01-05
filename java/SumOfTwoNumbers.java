96f6941884ce3b2fb1d3d772379c8ef292d4a427
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2024-01-05 06:48:07 +0530

    Update SumOfTwoNumbers.java

--- java/SumOfTwoNumbers.java
+++ java/SumOfTwoNumbers.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class SumOfTwoNumbers {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
         double firstNumber = scanner.nextDouble();
